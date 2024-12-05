cf9c64bb2604caa5f06ef69113d882366a6b5302
matgd <matgd@users.noreply.github.com>
2024-12-05 12:26:24 +0100

    D5 P1 refactor + P2 in progress

--- src/05/main.go
+++ src/05/main.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-const FILENAME = "input.txt"
+const FILENAME = "tinput.txt"
 
 type intSet map[int]struct{}
 type intIntSetMap map[int]intSet
@@ -63,20 +63,15 @@ func main() {
 			pageUpdates = append(pageUpdates, currentPageUpdates)
 		}
 	}
+	fmt.Println(rules)
 	part1(rules, pageUpdates)
+	// part2(rules, pageUpdates)
 }
 
 func part1(rules intIntSetMap, pageUpdates [][]int) {
 	validIndexes := make([]int, 0, 100)
 	for i, pageUpdate := range pageUpdates {
-		validOrder := true
-		for j := range pageUpdate {
-			if rules.containsAny(pageUpdate[j], pageUpdate[:j]) {
-				validOrder = false
-				break
-			}
-		}
-		if validOrder {
+		if orderIsValid(pageUpdate, rules) {
 			validIndexes = append(validIndexes, i)
 		}
 	}
@@ -85,5 +80,44 @@ func part1(rules intIntSetMap, pageUpdates [][]int) {
 	for _, vi := range validIndexes {
 		sum += pageUpdates[vi][len(pageUpdates[vi])/2]
 	}
-	fmt.Println("Part 1:", sum) // 5452
+	fmt.Println("Part 1:", sum)
+}
+
+func fixWithBubble(invalidOrder []int, invalidOrderIndexes []int) (validOrder []int) {
+	newOrder := make([]int, 0, 32)
+	copy(newOrder, invalidOrder)
+	fmt.Println("Invalid order:", invalidOrder)
+	fmt.Println("Invalid order indexes:", invalidOrderIndexes)
+
+	return newOrder
+}
+
+func orderIsValid(order []int, rules intIntSetMap) bool {
+	for i := range order {
+		if rules.containsAny(order[i], order[:i]) {
+			return false
+		}
+	}
+	return true
+}
+
+func part2(rules intIntSetMap, pageUpdates [][]int) {
+	// validIndexes := make([]int, 0, 100)
+	for _, pageUpdate := range pageUpdates {
+		validOrder := true
+		for j := range pageUpdate {
+			if rules.containsAny(pageUpdate[j], pageUpdate[:j]) {
+				validOrder = false
+				break
+			}
+		}
+		if validOrder {
+		}
+	}
+
+	sum := 0
+	for _, pageUpdate := range pageUpdates {
+		sum += pageUpdate[len(pageUpdate)/2]
+	}
+	fmt.Println("Part 2:", sum) // 5452
 }
