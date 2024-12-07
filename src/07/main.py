383198b8e6c2b6517501371e9288978dd927a716
matgd <matgd@users.noreply.github.com>
2024-12-07 14:28:48 +0100

    D7 P2 in progress

--- src/07/main.py
+++ src/07/main.py
@@ -1,7 +1,8 @@
 from itertools import product
 from dataclasses import dataclass
+import re
 
-INPUT_FILENAME = "input.txt"
+INPUT_FILENAME = "tinput.txt"
 
 @dataclass
 class Equation:
@@ -39,5 +40,32 @@ def part1(equations: list[Equation]):
     print(sum(possible_desired_results))
 
 
+def part2(equations: list[Equation]):
+    operators = ("+", "*", "||")
+    possible_desired_results = []
+    for equation in equations:
+        for op_combination in product(operators, repeat=len(equation.numbers) - 1):
+            raw_eq = []
+            for i, num in enumerate(equation.numbers):
+                raw_eq.append(num)
+                if i < len(op_combination):
+                    raw_eq.append(op_combination[i])
+            raw_eq = "".join(map(str, raw_eq))
+            raw_eq = raw_eq.replace("||", "")
+            eq = re.split(r"(\d+)", raw_eq)[1:-1]
+            print(eq)
+            result = equation.numbers[0]
+            for i, operator in enumerate(op_combination):
+                match operator:
+                    case "+":
+                        result += equation.numbers[i + 1]
+                    case "*":
+                        result *= equation.numbers[i + 1]
+            if result == equation.desired_result:
+                possible_desired_results.append(result)
+                break
+    print(sum(possible_desired_results))
+
 input = parse_input()
 part1(input)
+part2(input)
