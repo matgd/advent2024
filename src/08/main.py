572906c513045f92eb311e3126bba1dbb479a269
matgd <28411337+matgd@users.noreply.github.com>
2024-12-08 13:23:54 +0100

    D8 P2

--- src/08/main.py
+++ src/08/main.py
@@ -21,6 +21,18 @@ class AnthenaMap:
     def count_antinodes(self) -> int:
         return len(self.list_antinodes_locs())
 
+    def list_antinodes_locs_p2(self) -> list[Location]:
+        to_return = []
+        for row in self.locations:
+            for loc in row:
+                anthena_but_not_alone = len(self.anthenas_locations.get(loc.anthena, set())) > 1
+                if loc.antinode_of or anthena_but_not_alone:
+                    to_return.append(loc)
+
+        return to_return
+
+    def count_antinodes_p2(self) -> int:
+        return len(self.list_antinodes_locs_p2())
 
 def parse_input() -> AnthenaMap:
     with open(FILENAME, "r") as file:
@@ -66,21 +78,97 @@ def calc_antinodes_coords(loc_a: Location, loc_b: Location) -> tuple[tuple[int, 
         raise ValueError("In one line")
 
 
-def solution(input: AnthenaMap, part: int = 1) -> None:
+def part1(input: AnthenaMap) -> None:
     for anthena, locs in input.anthenas_locations.items():
         for i, loc_a in enumerate(locs):
             for loc_b in locs[i+1:]:
                 ((ca_y, ca_x), (cb_y, cb_x)) = calc_antinodes_coords(loc_a, loc_b)
                 if 0 <= ca_y < len(input.locations) and 0 <= ca_x < len(input.locations[0]):
                     input.locations[ca_y][ca_x].antinode_of.add(anthena)
-
                 if 0 <= cb_y < len(input.locations) and 0 <= cb_x < len(input.locations[0]):
                     input.locations[cb_y][cb_x].antinode_of.add(anthena)
 
     print("Part 1:", input.count_antinodes())
 
+def calc_antinodes_coords_p2(loc_a: Location, loc_b: Location, bounds_y: int, bounds_x: int) -> list[tuple[int, int]]:
+    x_diff = abs(loc_a.x - loc_b.x)
+    y_diff = abs(loc_a.y - loc_b.y)
+
+    to_return = []
+    if loc_a.x < loc_b.x and loc_a.y < loc_b.y:
+        new_loc_ay = loc_a.y - y_diff
+        new_loc_ax = loc_a.x - x_diff
+        while (0 <= new_loc_ay < bounds_y and 0 <= new_loc_ax < bounds_x):
+            to_return.append((new_loc_ay, new_loc_ax))
+            new_loc_ay = new_loc_ay - y_diff
+            new_loc_ax = new_loc_ax - x_diff
+
+        new_loc_ay = loc_a.y + y_diff
+        new_loc_ax = loc_a.x + x_diff
+        while (0 <= new_loc_ay < bounds_y and 0 <= new_loc_ax < bounds_x):
+            to_return.append((new_loc_ay, new_loc_ax))
+            new_loc_ay = new_loc_ay + y_diff
+            new_loc_ax = new_loc_ax + x_diff
+
+    elif loc_a.x < loc_b.x and loc_a.y > loc_b.y:
+        new_loc_ay = loc_a.y + y_diff
+        new_loc_ax = loc_a.x - x_diff
+        while (0 <= new_loc_ay < bounds_y and 0 <= new_loc_ax < bounds_x):
+            to_return.append((new_loc_ay, new_loc_ax))
+            new_loc_ay = new_loc_ay + y_diff
+            new_loc_ax = new_loc_ax - x_diff
+
+        new_loc_ay = loc_a.y - y_diff
+        new_loc_ax = loc_a.x + x_diff
+        while (0 <= new_loc_ay < bounds_y and 0 <= new_loc_ax < bounds_x):
+            to_return.append((new_loc_ay, new_loc_ax))
+            new_loc_ay = new_loc_ay - y_diff
+            new_loc_ax = new_loc_ax + x_diff
+
+    elif loc_a.x > loc_b.x and loc_a.y < loc_b.y:
+        new_loc_ay = loc_a.y - y_diff
+        new_loc_ax = loc_a.x + x_diff
+        while (0 <= new_loc_ay < bounds_y and 0 <= new_loc_ax < bounds_x):
+            to_return.append((new_loc_ay, new_loc_ax))
+            new_loc_ay = new_loc_ay - y_diff
+            new_loc_ax = new_loc_ax + x_diff
+
+        new_loc_ay = loc_a.y + y_diff
+        new_loc_ax = loc_a.x - x_diff
+        while (0 <= new_loc_ay < bounds_y and 0 <= new_loc_ax < bounds_x):
+            to_return.append((new_loc_ay, new_loc_ax))
+            new_loc_ay = new_loc_ay + y_diff
+            new_loc_ax = new_loc_ax - x_diff
+
+    elif loc_a.x > loc_b.x and loc_a.y > loc_b.y:
+        new_loc_ay = loc_a.y + y_diff
+        new_loc_ax = loc_a.x + x_diff
+        while (0 <= new_loc_ay < bounds_y and 0 <= new_loc_ax < bounds_x):
+            to_return.append((new_loc_ay, new_loc_ax))
+            new_loc_ay = new_loc_ay - y_diff
+            new_loc_ax = new_loc_ax - x_diff
+
+        new_loc_ay = loc_a.y - y_diff
+        new_loc_ax = loc_a.x - x_diff
+        while (0 <= new_loc_ay < bounds_y and 0 <= new_loc_ax < bounds_x):
+            to_return.append((new_loc_ay, new_loc_ax))
+            new_loc_ay = new_loc_ay - y_diff
+            new_loc_ax = new_loc_ax - x_diff
+    else:
+        raise ValueError("In one line")
+    return to_return
+
+def part2(input: AnthenaMap) -> None:
+    for anthena, locs in input.anthenas_locations.items():
+        for i, loc_a in enumerate(locs):
+            for loc_b in locs[i+1:]:
+                for cy, cx in calc_antinodes_coords_p2(loc_a, loc_b, len(input.locations), len(input.locations[0])):
+                    if 0 <= cy < len(input.locations) and 0 <= cx < len(input.locations[0]):
+                        input.locations[cy][cx].antinode_of.add(anthena)
+
+    print("Part 2:", input.count_antinodes_p2())
 
 input = parse_input()
 input_c = deepcopy(input)
-solution(input)  # 249
-# solution(input_c)
+part1(input)  # 249
+part2(input_c)  # 905
