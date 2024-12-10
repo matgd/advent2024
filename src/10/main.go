7a79ebcdc2f6c793e7c422a2947c82f25cfdb09e
matgd <matgd@users.noreply.github.com>
2024-12-10 13:14:53 +0100

    possible D10 P2?

---
+++ src/10/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func buildGreatWall(width int) []uint8 {
+	greatWall := make([]uint8, width, width)
+	for i := 0; i < width; i++ {
+		greatWall[i] = 99
+	}
+	return greatWall
+}
+
+func main() {
+	filename := os.Args[1]
+	input, _ := os.ReadFile(filename)
+	splitInput := strings.Split(string(input), "\n")
+	topology := [][]uint8{}
+	trailheadLocations := [][2]uint8{}
+	topology = append(topology, buildGreatWall(len(splitInput)+1))
+	for y, line := range splitInput {
+		yRow := []uint8{}
+		yRow = append(yRow, 99)
+		for x, xChar := range line {
+			if xChar == '.' {
+				yRow = append(yRow, 77)
+				continue
+			}
+			val := uint8(xChar - '0')
+			yRow = append(yRow, val)
+			if val == 0 {
+				trailheadLocations = append(trailheadLocations, [2]uint8{uint8(y + 1), uint8(x + 1)})
+			}
+		}
+		yRow = append(yRow, 99)
+		topology = append(topology, yRow)
+	}
+	topology = append(topology, buildGreatWall(len(splitInput)+1))
+	fmt.Printf("topology: %v\n", topology)
+
+	part1(topology, trailheadLocations)
+}
+
+func canReach9(topology [][]uint8, currentLocation [2]uint8, currentHeight uint8) int {
+	currY, currX := currentLocation[0], currentLocation[1]
+	for true {
+		// fmt.Println("start loc %v | curry currx %v,%v", currentLocation, currY, currX)
+		fmt.Printf("->%v,%v ", currY, currX)
+		if topology[currY][currX] == 9 {
+			fmt.Printf("start loc %v | found 9 %v,%v\n", currentLocation, currY, currX)
+			return 1
+		}
+		topDiff := topology[currY-1][currX] - currentHeight
+		leftDiff := topology[currY][currX-1] - currentHeight
+		rightDiff := topology[currY][currX+1] - currentHeight
+		bottomDiff := topology[currY+1][currX] - currentHeight
+		diffs := [4]uint8{topDiff, leftDiff, rightDiff, bottomDiff}
+		ones := [4]uint8{0, 0, 0, 0}
+		onesSum := 0
+		for i, diff := range diffs {
+			if diff == 1 {
+				ones[i] = 1
+				onesSum++
+			}
+		}
+
+		if onesSum > 1 {
+			topFound, leftFound, rightFound, bottomFound := 0, 0, 0, 0
+			if topDiff == 1 {
+				topFound = canReach9(topology, [2]uint8{currY - 1, currX}, currentHeight+1)
+			}
+			if leftDiff == 1 {
+				leftFound = canReach9(topology, [2]uint8{currY, currX - 1}, currentHeight+1)
+			}
+			if rightDiff == 1 {
+				rightFound = canReach9(topology, [2]uint8{currY, currX + 1}, currentHeight+1)
+			}
+			if bottomDiff == 1 {
+				bottomFound = canReach9(topology, [2]uint8{currY + 1, currX}, currentHeight+1)
+			}
+			return topFound + leftFound + rightFound + bottomFound
+		} else if onesSum == 1 {
+			if topDiff == 1 {
+				currY = currY - 1
+			} else if leftDiff == 1 {
+				currX = currX - 1
+			} else if rightDiff == 1 {
+				currX = currX + 1
+			} else if bottomDiff == 1 {
+				currY = currY + 1
+			}
+			currentHeight++
+		} else {
+			return 0
+		}
+	}
+	return 0
+}
+
+func part1(topology [][]uint8, trailheadLocations [][2]uint8) {
+	hikingTrails := 0
+	fmt.Printf("trailheadLocations: %v\n", trailheadLocations)
+	for _, yx := range trailheadLocations {
+		hikingTrails += canReach9(topology, yx, 0)
+	}
+	fmt.Printf("hikingTrails: %v\n", hikingTrails)
+}
