41b0c4e72817990570e0d395b702c6524f581e28
matgd <matgd@users.noreply.github.com>
2024-12-13 11:36:34 +0100

    D13 P1

---
+++ src/13_c/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#define INF 999999
+
+int main(int argc, char *argv[]) 
+{ 
+    FILE *fptr = fopen(argv[1], "r");
+    int ax[400], ay[400], bx[400], by[400], px[400], py[400];
+
+    int i = 0;
+    for (; i < 400; i++)
+    {
+        ax[i] = 0;
+        ay[i] = 0;
+        bx[i] = 0;
+        by[i] = 0;
+        px[i] = 0;
+        py[i] = 0;
+    }
+
+    i = 0;
+    do 
+    {
+        fscanf(fptr, "Button A: X+%d, Y+%d\n", &ax[i], &ay[i]);
+        fscanf(fptr, "Button B: X+%d, Y+%d\n", &bx[i], &by[i]);
+        fscanf(fptr, "Prize: X=%d, Y=%d\n", &px[i], &py[i]);
+        i++;
+    } while (!feof(fptr));
+    fclose(fptr);
+    
+    int tokensspent = 0;
+    for (int j = 0; j < i; j++)
+    {
+        int mincost = INF;
+        for (int a = 0; a < 100; a++)
+        {
+            for (int b = 0; b < 100; b++)
+            {
+                /*printf("%d %d\n", a, b);*/
+                int posx = a * ax[j] + b * bx[j];
+                int posy = a * ay[j] + b * by[j];
+                if (posx == px[j] && posy == py[j])
+                {
+                    int cost = a * 3 + b;
+                    if (cost < mincost)
+                    {
+                        mincost = cost;
+                    }
+                }
+            }
+        }
+        if (mincost < INF)
+        {
+            tokensspent += mincost;
+        }
+    }
+    printf("Tokens spent: %d\n", tokensspent);
+}
